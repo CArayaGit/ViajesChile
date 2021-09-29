91a5554e78988e97230c6bf3f9096e3f6f68572e
CArayaGit <c.araya.aliaga@gmail.com>
2021-09-28 22:37:58 -0400

    Se incorpora modal y d-none seccion quienes somos

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,9 @@ $(function(){
         }
     });
 
-    
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+     return new bootstrap.Popover(popoverTriggerEl)
+    })
 
 })
