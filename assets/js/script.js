685720602b1857b12a67198aaf9e3bca19c1df34
CArayaGit <c.araya.aliaga@gmail.com>
2021-09-30 20:58:18 -0400

    se incorporan comentarios

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,12 +13,13 @@ $(function(){
         }
     });
 
-
+    //para popover
     var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
     var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
      return new bootstrap.Popover(popoverTriggerEl)
     })
 
+    //para toast, popup asistente virtual
     var toastTrigger = document.getElementById('liveToastBtn')
     var toastLiveExample = document.getElementById('liveToast')
     if (toastTrigger) {
@@ -28,7 +29,6 @@ $(function(){
         toast.show()
     })
     }
-
 
 })
 
